deeb82449d20bc75e8973097218a29c144417df2
Seth Fitzsimmons <seth@mojodna.net>
2013-09-20 17:01:02 -0700

    TnT segment matching and ordering

--- sql/segment_tools.sql
+++ sql/segment_tools.sql
@@ -1,3 +1,178 @@
+-- -d parks
+
+CREATE OR REPLACE FUNCTION PC_MatchSegments(path geometry)
+RETURNS TABLE (ogc_fid integer, trail_name varchar, geom geometry)
+AS $$
+DECLARE
+  candidate RECORD;
+  first geometry;
+  last geometry;
+  f_dist float;
+  l_dist float;
+BEGIN
+  FOR candidate IN SELECT trails.ogc_fid, trails.trail_name, (ST_Dump(trails.geom)).geom
+                   FROM trails3d trails
+                   WHERE ST_DWithin(trails.geom, path, 100)
+  LOOP
+    RAISE NOTICE '%', candidate.trail_name;
+
+    first := ST_PointN(candidate.geom, 1);
+    last := ST_PointN(candidate.geom, ST_NumPoints(candidate.geom));
+
+    f_dist := ST_Distance(path, first);
+    l_dist := ST_Distance(path, last);
+
+    RAISE NOTICE 'Distance to first point: %', f_dist;
+    RAISE NOTICE 'Distance to last point: %', l_dist;
+
+    IF f_dist < 50 AND l_dist < 50 THEN
+      RETURN QUERY SELECT candidate.ogc_fid, candidate.trail_name, candidate.geom;
+    END IF;
+  END LOOP;
+END
+$$ LANGUAGE plpgsql;
+
+-- WITH matched_points AS (
+--   SELECT row_number() OVER () idx,
+--     (SELECT matched.ogc_fid
+--       FROM matched
+--       WHERE ST_Dwithin(matched.geom, points.geom, 100)
+--       ORDER BY ST_Distance(matched.geom, points.geom) ASC
+--       LIMIT 1) ogc_fid
+--     FROM (
+--       SELECT (ST_DumpPoints(geom)).geom
+--       FROM tnt_trips
+--     ) AS points
+-- )
+-- SELECT ids.idx, ids.ogc_fid
+-- FROM (
+--   SELECT 0 idx, (SELECT matched_points.ogc_fid FROM matched_points ORDER BY matched_points.idx LIMIT 1) ogc_fid
+--   UNION
+--   SELECT ids.idx, ids.ogc_fid
+--   FROM (
+--     SELECT windowed_ids.idx,
+--       windowed_ids.ogc_fid
+--     FROM (
+--       SELECT matched_points.idx,
+--         matched_points.ogc_fid, first_value(matched_points.ogc_fid) OVER w prev_fid
+--       FROM matched_points
+--       WINDOW w AS (ROWS 1 PRECEDING)
+--       ORDER BY matched_points.idx
+--     ) AS windowed_ids
+--     WHERE windowed_ids.ogc_fid <> prev_fid
+--   ) AS ids
+-- ) AS ids
+-- ;
+
+CREATE OR REPLACE FUNCTION PC_OrderedSegments(path geometry)
+-- TODO return ogc_fid and trail_name so that we have (ordered) names for
+-- things
+-- RETURNS TABLE (idx bigint, ogc_fid integer, trail_name varchar, geom geometry)
+RETURNS geometry
+AS $$
+DECLARE
+  match RECORD;
+  segment geometry;
+  line geometry;
+  start geometry;
+  L geometry;
+  R geometry;
+  l_dist float;
+  r_dist float;
+BEGIN
+  DROP TABLE IF EXISTS matched;
+  DROP TABLE IF EXISTS ordered_segment_ids;
+
+  CREATE TEMPORARY TABLE matched ON COMMIT DROP AS
+    SELECT row_number() OVER () idx, *
+    FROM (
+      SELECT (PC_MatchSegments(path)).*
+    ) AS _
+    ;
+
+  CREATE TEMPORARY TABLE ordered_segment_ids ON COMMIT DROP AS
+    WITH matched_points AS (
+      SELECT row_number() OVER () idx,
+        (SELECT matched.ogc_fid
+          FROM matched
+          WHERE ST_Dwithin(matched.geom, points.geom, 100)
+          ORDER BY ST_Distance(matched.geom, points.geom) ASC
+          LIMIT 1) ogc_fid
+        FROM (
+          SELECT (ST_DumpPoints(path)).geom
+        ) AS points
+    )
+    SELECT ids.idx, ids.ogc_fid
+    FROM (
+      SELECT 0 idx, (SELECT matched_points.ogc_fid FROM matched_points ORDER BY matched_points.idx LIMIT 1) ogc_fid
+      UNION -- instead of unioning, window w/ prev, current, next.  if prev + next are the same, remove current and next
+      SELECT ids.idx, ids.ogc_fid
+      FROM (
+        SELECT windowed_ids.idx,
+          windowed_ids.ogc_fid
+        FROM (
+          SELECT matched_points.idx,
+            matched_points.ogc_fid, first_value(matched_points.ogc_fid) OVER w prev_fid
+          FROM matched_points
+          WINDOW w AS (ROWS 1 PRECEDING)
+          ORDER BY matched_points.idx
+        ) AS windowed_ids
+        WHERE windowed_ids.ogc_fid <> prev_fid
+      ) AS ids
+    ) AS ids
+    ;
+
+    -- TODO pass through with another window function to eliminate segments
+    -- that don't connect neighbors
+
+    -- TODO I really want geometries here
+    SELECT matched_points.ogc_fid,
+      first_value(matched_points.ogc_fid) OVER w prev_fid,
+      last_value(matched_points.ogc_fid) OVER w next_fid,
+
+    RAISE NOTICE 'ordered segment ids: %', (SELECT array_agg(ogc_fid) FROM (SELECT ogc_fid FROM ordered_segment_ids ORDER BY idx) AS _);
+
+    start := ST_PointN(path, 1);
+
+    FOR match IN SELECT *
+      FROM ordered_segment_ids
+      ORDER BY ordered_segment_ids.idx
+    LOOP
+      RAISE NOTICE 'ogc_fid: %', match.ogc_fid;
+
+      segment := (
+        SELECT (ST_Dump(trails.geom)).geom
+        FROM trails3d trails
+        WHERE trails.ogc_fid = match.ogc_fid
+      );
+
+      RAISE NOTICE 'geometries: %', ST_NumGeometries(segment);
+
+      L := ST_PointN(segment, 1);
+      R := ST_PointN(segment, ST_NumPoints(segment));
+      l_dist := ST_Distance(start, L);
+      r_dist := ST_Distance(start, R);
+
+      IF r_dist > l_dist THEN
+        start := R;
+      ELSE
+        RAISE NOTICE 'reversing';
+
+        start := L;
+        segment := ST_Reverse(segment);
+      END IF;
+
+      IF line IS NULL THEN
+        line := ST_MakeLine(segment);
+      ELSE
+        line := ST_MakeLine(line, segment);
+      END IF;
+    END LOOP;
+
+    RETURN line;
+END
+$$ language plpgsql;
+
 CREATE OR REPLACE FUNCTION PC_OrderSegments(segments geometry[], start geometry)
 RETURNS geometry
 AS $$
@@ -124,7 +299,7 @@ DECLARE
   points geometry[];
 BEGIN
   points := (SELECT array_agg(geom) FROM (
-              SELECT ST_MakePoint(sum(dx) OVER (ORDER BY idx) * 3.28, z * 3.28) geom
+              SELECT ST_MakePoint(sum(dx) OVER (ORDER BY idx), z) geom
               FROM (
                 SELECT row_number() OVER () idx,
                 ST_Distance(first_value(geom) OVER (ROWS BETWEEN 1 PRECEDING AND CURRENT ROW), geom) dx,
