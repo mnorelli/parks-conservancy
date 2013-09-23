27a64af5f54235a320c3e8512fe312b62d849a21
Shawn Allen <shawn@stamen.com>
2013-09-23 14:57:59 -0700

    +d3.jsonp, fixes

--- play/travel-time/js/travel-time.js
+++ play/travel-time/js/travel-time.js
@@ -32,31 +32,60 @@
       mtc.blockFindUrl, "?",
       "longitude=", loc[0], "&",
       "latitude=", loc[1], "&",
-      "format=json"
+      "format=jsonp&callback={callback}"
     ].join("");
-    return d3.json(url, function(error, data) {
-      if (error) {
-        return callback(error, data);
-      } else if (data.status === "OK") {
-        var fips = data.Block.FIPS;
-        return callback(null, fips);
-      } else {
-        if (data.Err) {
+    return d3.jsonp(url, function(data) {
+      if (data) {
+        if (data.status === "OK") {
+          var fips = data.Block.FIPS;
+          return callback(null, fips);
+        } else if (data.Err) {
           return callback(data.Err);
         }
       }
-      return callback(data);
+      return callback(null, data);
     });
   };
 
   mtc.location2taz = function(loc, callback) {
     return mtc.location2fips(loc, function(error, fips) {
       if (fips) {
+        console.log("loc -> fips:", loc, "->", fips);
         return mtc.blk2taz(fips, callback);
       } else {
+        console.warn("error:", error);
         return callback(error);
       }
     });
   };
+
+  if (!d3.jsonp) {
+    d3.jsonp = function(url, callback) {
+      function rand() {
+        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+            c = '',
+            i = -1;
+        while (++i < 15) c += chars.charAt(Math.floor(Math.random() * 52));
+        return c;
+      }
+
+      function create(url) {
+        var e = url.match(/callback=d3.jsonp.(\w+)/),
+            c = e ? e[1] : rand();
+        d3.jsonp[c] = function(data) {
+          callback(data);
+          delete d3.jsonp[c];
+          script.remove();
+        };
+        return 'd3.jsonp.' + c;
+      }
+
+      var cb = create(url),
+        script = d3.select('head')
+        .append('script')
+        .attr('type', 'text/javascript')
+        .attr('src', url.replace(/(\{|%7B)callback(\}|%7D)/, cb));
+    };
+  }
 
 })(this);
