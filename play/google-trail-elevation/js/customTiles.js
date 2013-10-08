34559e3ea8f8397287d2108af1e4f10b30be8622
miketahani <tahani@stamen.com>
2013-10-08 13:39:52 -0700

    seth's new tiles

--- play/google-trail-elevation/js/customTiles.js
+++ play/google-trail-elevation/js/customTiles.js
@@ -4,10 +4,22 @@
 var mapTypeOptions = {
   getTileUrl: function(coord, zoom) {
       var bound = Math.pow(2, zoom);
-      var url = "http://map.parks.stamen.com" +
-          "/" + zoom + "/" + coord.x + "/" +
-          coord.y + ".png";
-      // console.log("tile url:", url);
+      var url = [
+        "http://sm.mapstack.stamen.com/",
+        "((http%3A%2F%2Ftilefarm.stamen.com",
+        "%2Fterrain-grey-hills%2F%7Bz%7D%2F%7Bx%7D%2F%7By%7D.png,",
+        "$fff[@60],$fff[hsl-saturation])[@35],",
+        "http%3A%2F%2Fggnpc-tp.herokuapp.com%2Fbackground",
+        "%2F%7Bz%7D%2F%7Bx%7D%2F%7By%7D.png[multiply]),",
+        "http%3A%2F%2Fggnpc-tp.herokuapp.com",
+        "%2Ffeatures%2F%7Bz%7D%2F%7Bx%7D%2F%7By%7D.png,",
+        "http%3A%2F%2Fggnpc-tp.herokuapp.com",
+        "%2Flabels%2F%7Bz%7D%2F%7Bx%7D%2F%7By%7D.png",
+        "/" + zoom + "/" + coord.x + "/" + coord.y + ".png"
+      ].join("");
+      // var url = "http://map.parks.stamen.com" +
+      //     "/" + zoom + "/" + coord.x + "/" +
+      //     coord.y + ".png";
       return url;
   },
   tileSize: new google.maps.Size(256, 256),
