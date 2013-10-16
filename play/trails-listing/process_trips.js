352e9c0d44bf027a3038386aba5ebb4de4a9981f
miketahani <tahani@stamen.com>
2013-10-16 12:39:23 -0700

    node script to pull tnt meta and trip data and combine it with mapquest elevation data

---
+++ play/trails-listing/process_trips.js
@@ -0,0 +1,103 @@
+// node script to grab and process T&T trips data
+var TransitAndTrails = require("transitandtrails");
+var fs = require("fs"),
+    http = require("http"),
+    request = require("request");
+
+var tnt = new TransitAndTrails({
+  key: "267de82bc6c6c1c56ac5c10dfc0f50e42dde75810f4b32c1c0c4db012d3f22d4"
+});
+
+var mapquest = {
+  key: "Fmjtd%7Cluub2q61n5%2Crx%3Do5-9610g6"
+};
+mapquest.host = "http://open.mapquestapi.com";
+mapquest.path = "/elevation/v1/profile?key=" + mapquest.key + "&unit=f";
+
+
+var trips = [
+  1287, 1424, 1431, 89, 45, 1297, 216, 
+  1338, 1446, 1295, 140, 1333, 63, 1448, 
+  1449, 263, 1433
+];
+
+function writeFile(data, filename) {
+  // var filename = "tnt-geojson/" + id + ".geojson";
+  fs.exists(filename, function(exists) {
+    if (!exists) {
+      fs.writeFile(filename, JSON.stringify(data, null, 2), function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("[*] data written to file", filename);
+        }
+      });
+    } else {
+      console.log("[*] file exists, skipping");
+      // console.log("[*] tnt data for " + id + " already exists. skipping.");
+    }
+  });
+}
+
+// grab raw trips data from T&T
+// XXX TODO: LOG ERRORS TO A FILE SO WE CAN RERUN THIS WHOLE PROCESS
+trips.forEach(function(id) {
+  
+  console.log("[*] starting", id);
+  
+  tnt.getTrip(id, function(err, trip) {
+    if (err) {
+      console.error(err);
+    }
+
+    console.log("[*] got trip metadata from TnT");
+
+    tnt.getTripRoute(id, function(err, route) {
+      console.log("[*] got TnT trip coordinates");
+
+      var route = route.map(function(d) { return d.reverse(); });
+
+      var data = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "LineString",
+              coordinates: route
+            },
+            properties: trip
+          }
+        ]
+      };
+      // data.features[0].properties.trip.tnt_id = id;
+      var filename = "trips/" + id + ".geojson";
+      writeFile(data, filename);
+      
+      var latLngCollection = route.map(function(d) { return d.join(","); }).join(",");
+      var mapquestUrl = mapquest.host + mapquest.path;
+
+      request.post({
+        url: mapquestUrl,
+        form: {
+          latLngCollection: latLngCollection
+        }
+      }, function(err, res, body) {
+        
+        console.log("[*] got mapquest elevation data");
+        
+        var elevation = JSON.parse(body).elevationProfile;
+        elevation = elevation.map(function(d, i) { 
+          d.coordinates = route[i];
+          return d;
+        });
+        var profile = {elevation: elevation, metadata: trip};
+        var filename = "trips/" + id + "-elevation.json";
+        // XXX TODO: THIS NEEDS TO GO IN A DATABASE OR SOMETHING
+        writeFile(profile, filename);
+      });
+
+      // console.log("%j", data);
+    });
+  });
+});
