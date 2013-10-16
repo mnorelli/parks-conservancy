2c48ebe55d4520827529dc42408708fc29611c52
miketahani <tahani@stamen.com>
2013-10-16 15:56:14 -0700

    test.sh to get_trails.sh

--- play/trails-listing/test.sh
+++
@@ -1,8 +0,0 @@
-for trip in trips/*-elevation.json 
-do
-  trip=${trip%-elevation.json}
-  trip=${trip#trips/}
-  echo $trip
-  node d3-to-png/index.js --trip $trip ../svg_trail_paths.js > svg/$trip.svg
-  convert -background none svg/$trip.svg png/$trip.png
-done
