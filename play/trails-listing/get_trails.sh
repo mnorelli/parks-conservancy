c5b7f49b9f4d56ccfc3b22a6d5b40729bcc70192
miketahani <tahani@stamen.com>
2013-10-17 12:02:04 -0700

    debug

--- play/trails-listing/get_trails.sh
+++ play/trails-listing/get_trails.sh
@@ -8,5 +8,5 @@ mkdir -p svg png
 for id in $tripids; do
   echo $id
   node d3-to-png/index.js --trip $id ../svg_trail_paths.js > svg/$id.svg
-  convert -background none svg/$id.svg png/$id.png
+  convert -verbose -debug all -background none svg/$id.svg png/$id.png
 done
