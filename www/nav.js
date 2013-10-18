e99cad040aeee0712face003d9c774b6ce7dced6
Shawn Allen <shawn@stamen.com>
2013-10-17 20:08:16 -0700

    add custom nav at the end, make it pink
    /cc #147

--- www/nav.js
+++ www/nav.js
@@ -24,20 +24,25 @@
 
       // the "Your Visit" menu is the second "upper" bit,
       // and ul.level1 is where links get attached
-      var menu = d3.select("#main_menu .upper:nth-child(2) ul.level1");
+      var menu = d3.select("#main_menu .upper:nth-child(2)"),
+          hook = menu.select(".column2 ul.level1");
+
+      // add the Map link
+      hook.append("li")
+        .attr("class", "stamen map")
+        .append("a")
+          .attr("href", "/map/")
+          .text("Map");
 
       // add the Trip Planner link
-      menu.insert("li", "li:first-child")
+      hook.append("li")
+        .attr("class", "stamen planner")
         .append("a")
           .attr("href", "/map/planner/")
           .text("Plan a Trip");
 
-      // add the Map link
-      menu.insert("li", "li:first-child")
-        .attr("class", "map")
-        .append("a")
-          .attr("href", "/map/")
-          .text("Map");
+      hook.selectAll(".stamen a")
+        .style("color", "#f0f");
     }
   }
 
